55eea6dab7bee877d11c8e288c37de87ab9b5446
ELESSAR <1105472890@qq.com>
2018-12-10 08:39:41 +0800

    Initial commit

---
+++ TensorFlow-GPU/TEST.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+a = tf.random_normal((100, 100))
+b = tf.random_normal((100, 500))
+c = tf.matmul(a, b)
+sess = tf.InteractiveSession()
---
+++ TensorFlow/test.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+a = tf.random_normal((100, 100))
+b = tf.random_normal((100, 500))
+c = tf.matmul(a, b)
+sess = tf.InteractiveSession()
+print('aaa')
